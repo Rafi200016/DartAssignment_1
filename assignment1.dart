2cd2319ae893450c5ebfb4eab5012eb1010c6559
rafi200016 <ahsanulrafi16@gmail.com>
2024-03-15 00:13:59 +0600

    Updated Assignment

---
+++ assignment1.dart
@@ -0,0 +1,26 @@
+class Media {
+  //Class created
+  void play()
+  {
+    print('playing media..');
+  }
+}
+
+class Song extends Media {
+  String artist;
+  
+  Song(this.artist);
+  @override
+  void play() {
+    print('playing song by $artist');
+  }
+}
+
+void main()
+{
+Media media = Media();
+Song song = Song('Ronaldo') ;
+media.play();
+song.play();
+
+}
